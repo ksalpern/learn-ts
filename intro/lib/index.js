518b9d1e971580aa4d33998480a124398a529233
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-26 21:09:43 +0300

    working with class

--- intro/lib/index.js
+++ intro/lib/index.js
@@ -1,3 +1,16 @@
 "use strict";
-let message = 'Hello World';
-console.log(message);
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    move(distance = 0) {
+        console.log(`${this.name} moved ${distance}m.`);
+    }
+}
+let cat = new Animal("Cat");
+cat.move(10);
+class Bird extends Animal {
+    fly(distance = 0) {
+        console.log(`${this.name} fly ${distance}m.`);
+    }
+}
--- intro/src/index.ts
+++ intro/src/index.ts
@@ -1,2 +1,18 @@
-let message: string = 'Hello World';
-console.log(message);
+class Animal {
+  protected name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  public move(distance: number = 0): void {
+    console.log(`${this.name} moved ${distance}m.`);
+  }
+}
+
+let cat = new Animal("Cat");
+cat.move(10);
+
+class Bird extends Animal {
+  fly(distance: number = 0): void {
+    console.log(`${this.name} fly ${distance}m.`);
+  }
+}
