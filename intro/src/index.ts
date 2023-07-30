341d2a38255753160406996d4feacee83a3d86cc
Kseniia Pidopryhora <ksenia.pidopryhora@gmail.com>
2023-07-30 13:31:34 +0300

    dealing with Temporal Uncertainty

--- intro/src/index.ts
+++ intro/src/index.ts
@@ -1,16 +1,19 @@
-type Person = {
-  name: string;
-  email?: string | null | undefined;
-};
-
-function sendEmail(email: string): void {
-  console.log(`send email to ${email}!`);
+let suffix: string | null = getSuffix();
+if (suffix !== null) {
+  const suffixLocal = suffix;
+  let exampleOne: string = `Hello, ${suffixLocal.toLocaleLowerCase()}!`;
+  ["jane", "mark", "paul"].forEach((name) => {
+    let exampleTwo: string = `Hello, ${name} ${suffixLocal}!`;
+    console.log(exampleTwo);
+  });
 }
 
-function contact(person: Person): void {
-  console.log(`contact ${person.name} at ${person.email}`);
-  if (person.email === null || person.email === undefined) {
-    throw new Error("Email is missing!");
-  }
-  sendEmail(person.email);
+let example: string | null = forExample();
+if (example !== null) {
+  const exampleLocal = example;
+  setTimeout(() => {
+    console.log(exampleLocal.toLocaleLowerCase());
+  }, 1000);
 }
+
+example = null;
